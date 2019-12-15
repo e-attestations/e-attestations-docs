7a9ed44e01eb243b30f3f183ae911a22e8e664d0
Vincent <vincent.dagoury@gmail.com>
2019-12-15 21:18:15 +0100

    first commit

---
+++ docusaurus.config.js
@@ -0,0 +1,86 @@
+/**
+ * Copyright (c) 2017-present, Facebook, Inc.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+module.exports = {
+  title: 'API e-Attestations',
+  tagline: 'Connect your systems with e-Attestations',
+  url: 'https://www.e-attestations.com',
+  baseUrl: '/',
+  favicon: 'img/favicon.png',
+  organizationName: 'e-attestations', // Usually your GitHub org/user name.
+  projectName: 'e-attestations-docs', // Usually your repo name.
+  themeConfig: {
+    navbar: {
+      title: '',
+      logo: {
+        alt: 'e-Attestations',
+        src: 'img/cropped-logo-eattestations-2.png',
+      },
+      links: [
+        {to: 'docs/doc2', label: 'QuickStart', position: 'left'},
+        {to: 'docs/doc1', label: 'Docs', position: 'left'},
+        {to: 'blog', label: 'Blog', position: 'left'},
+        {
+          href: 'https://github.com/e-attestations',
+          label: 'GitHub',
+          position: 'right',
+        },
+      ],
+    },
+    footer: {
+      style: 'dark',
+      links: [
+        {
+          title: 'Docs',
+          items: [
+            {
+              label: 'Docs',
+              to: 'docs/doc1',
+            },
+          ],
+        },
+        {
+          title: 'Community',
+          items: [
+            {
+              label: 'Discord',
+              href: 'https://discordapp.com/invite/docusaurus',
+            },
+          ],
+        },
+        {
+          title: 'Social',
+          items: [
+            {
+              label: 'Blog',
+              to: 'blog',
+            },
+          ],
+        },
+      ],
+      logo: {
+        alt: 'Facebook Open Source Logo',
+        src: 'https://docusaurus.io/img/oss_logo.png',
+        href: 'https://opensource.facebook.com/',
+      },
+      copyright: `Copyright © ${new Date().getFullYear()} Facebook, Inc. Built with Docusaurus.`,
+    },
+  },
+  presets: [
+    [
+      '@docusaurus/preset-classic',
+      {
+        docs: {
+          sidebarPath: require.resolve('./sidebars.js'),
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      },
+    ],
+  ],
+};
